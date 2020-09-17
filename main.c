9fae51f48284a2fa333c32cbf9a6b55f33988595
YanLu <yfl5541@psu.edu>
2020-09-17 18:45:55 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,37 @@
+// Author: Yan Lu yfl5541@psu.edu
+// Collaborator: Spoorthi Dhulappanavar sxd5682@psu.edu
+// Collaborator: Nicole Giron nqg5259@psu.edu
+// Collaborator: Xiaolong Lin xxl5453@psu.edu
+// Section: 4
+// Breakout: 11
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int sum_n(int n) {
+  if ( n>0 ) {
+    return n+sum_n(n-1);
+  }
+  else {
+    return n;
+  }
+}
+
+void print_n(const char *s, int n) {
+  if (n>0) {
+    printf("%s\n",s);
+    print_n(s,n-1);
+  }
+  else {
+    printf("");
+  }
+}
+
+int main(void) {
+  int n = atof(readline("Enter an int: "));
+  printf("sum is %d. \n", sum_n(n));
+  const char *b = readline("Enter a string: ");
+  print_n(b, n);
+
+  return 0;
+}
